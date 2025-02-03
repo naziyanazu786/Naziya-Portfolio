10fcc0f6a60076436356ffea18bcea9df3b6c4a1
Naziya <snaziya490@gmail.com>
2025-02-03 13:37:41 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,4 @@ if os.environ.get('FLASK_ENV') == 'production':
     app.config.from_object(ProductionConfig)
 else:
     app.config.from_object(DevelopmentConfig)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
     
