7be0651fd3f709612b4a81bad314e5006a2b6cff
Naziya <snaziya490@gmail.com>
2025-02-03 13:36:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+import os
 
 app = Flask(__name__)
 
@@ -43,4 +44,5 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
